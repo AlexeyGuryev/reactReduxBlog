6a433d6fba3d2f5b4eeac36088a8a960be1f248c
Alexey Guryev <alguryew@gmail.com>
2017-02-15 22:52:19 +0300

    New post

---
+++ src/components/posts_new.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
+
+class PostsNew extends Component {
+    render() {
+        const { fields: { title, categories, content }, handleSubmit } = this.props;
+        //equals:
+        //const handleSubmit = this.props.handleSubmit;
+        //const title = this.props.fields.title;
+        
+        return (
+            <form onSubmit={handleSubmit(this.props.createPost)}>
+                <h3>Create a New Post</h3>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input type="text" className="form-control" {...title}/>
+                    {/*equals <input type="text" className="form-control" formProps={title}/>*/}
+                </div>                
+
+                <div className="form-group">
+                    <label>Categories</label>
+                    <input type="text" className="form-control" {...categories}/>
+                </div>                
+
+                <div className="form-group">
+                    <label>Content</label>
+                    <textarea className="form-control" {...content}/>
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        );
+    }
+}
+// reduxForm like connect, but 1st arg is form config
+// 2nd arg is mapStateToProps
+// 3rd arg is mapDispatchToProps
+export default reduxForm({
+    form: 'PostsNewForm',
+    fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);

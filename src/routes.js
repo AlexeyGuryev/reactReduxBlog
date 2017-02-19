aa547939835530b6c93f7d0e5f2061c79dd3ebe2
Alexey Guryev <alguryew@gmail.com>
2017-02-19 21:42:46 +0300

    Posts list and deleting

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsView from './components/posts_view';
 
 export default (
     <Route path="/" component={App} >
         <IndexRoute component={PostsIndex} />
         <Route path="posts/new" component={PostsNew} />
+        <Route path="posts/:id" component={PostsView} /> 
     </Route>
 );
